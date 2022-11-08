8429dc0dca866aa46beab0c59717c31e6e15e7dd
unknown <mdyasirofficial2001@gmail.com>
2022-11-09 05:01:31 +0600

    routes created

--- index.js
+++ index.js
@@ -37,6 +37,13 @@ async function run() {
       const services = await cursor.toArray();
       res.send(services);
     });
+
+    app.get("/servicesforhome", async (req, res) => {
+      const query = {};
+      const cursor = await serviceCollection.find(query);
+      const services = await cursor.limit(3).toArray();
+      res.send(services);
+    });
   } finally {
   }
 }
