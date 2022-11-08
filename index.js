37ad1c639d15128f7590906a734be75e874299e0
unknown <mdyasirofficial2001@gmail.com>
2022-11-09 03:07:00 +0600

    Services Loaded to the server

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -15,3 +17,27 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Dr. Teeth server running in localhost:${port}`);
 });
+
+// Mongo Connect
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uue1axy.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const serviceCollection = client.db("dr-teeth").collection("services");
+
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = await serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } finally {
+  }
+}
+run().catch((e) => console.log(e));
